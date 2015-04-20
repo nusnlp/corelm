427dac3fa22e5f7d6e69eb336ed80d8070f2a7b3
Shamil CM <shamil.cm@gmail.com>
2015-04-20 17:22:42 +0800

    Argparse for memmap_dataset.pywq

--- memmap_dataset.py
+++ memmap_dataset.py
@@ -5,16 +5,25 @@ import theano
 import sys, os
 import tempfile
 import shutil
+import argparse
 
-if len(sys.argv) != 5:
-	# U.usage()
-	print "USAGE: python " + sys.argv[0] + " input_txt ngram output_idx output_txt('-' for null)"
-	sys.exit()
 
-input_path = sys.argv[1]				# Input text file
-ngram = int(sys.argv[2])				# Order of Ngram
-output_path = sys.argv[3]				# Binarized output filename of ngram indices
-output_path_txt = sys.argv[4]			# Text output filename of ngram indices (optional)
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", dest="input_path", required=True, help="Path to the input text file.")
+parser.add_argument("-n", "--ngram-size", dest="ngram_size", required=True, type=int, help="Ngram Size.")
+parser.add_argument("-o", "--output", dest="output_path", help="Path to memory mapped output file.")
+parser.add_argument("-t", "--output-text", dest="output_text_path", help="Path to text output file.")
+
+args = parser.parse_args()
+
+# Set the default output path, if None is specified
+if args.output_path is None:
+	args.output_path = args.input_path + ".idx.mmap"
+
+input_path = args.input_path			# Input text file path
+output_path = args.output_path			# Output Path for memory mapped ngrams indices file
+output_path_txt = args.output_text_path	# Output Path for text ngrams indices file
+ngram_size = args.ngram_size			# Ngram Size
 vocab_path = input_path+".vocab"		# Vocab path 
 
 nsamples = 0
@@ -30,7 +39,7 @@ with open(input_path, 'r') as input_file, open(tmp_path, 'w') as tmp_file:
 		if len(line) == 0:
 			continue
 		tokens = line.strip().split()
-		for i in range(ngram - 1):
+		for i in range(ngram_size - 1):
 			tokens.insert(0, '<s>')
 		indices = []
 		for token in tokens:
@@ -41,8 +50,8 @@ with open(input_path, 'r') as input_file, open(tmp_path, 'w') as tmp_file:
 			else:
 				word_to_freq_dict[token] += 1
 			indices.append(word_to_id_dict[token])
-		for i in range(ngram - 1, len(indices)):
-			tmp_file.write(' '.join(indices[i - ngram + 1 : i + 1]) + "\n")
+		for i in range(ngram_size - 1, len(indices)):
+			tmp_file.write(' '.join(indices[i - ngram_size + 1 : i + 1]) + "\n")
 			nsamples += 1
 
 # Writing to the vocabulary file in decreasing order of frequency
@@ -51,9 +60,9 @@ with  open(vocab_path,'w') as f_vocab:
 		f_vocab.write(token+"\t"+ word_to_id_dict.get(token)+"\n")
 
 with open(tmp_path, 'r') as data:
-	fp = np.memmap(output_path, dtype='int32', mode='w+', shape=(nsamples + 1, ngram))
+	fp = np.memmap(output_path, dtype='int32', mode='w+', shape=(nsamples + 1, ngram_size))
 	fp[0,0] = nsamples					# number of samples
-	fp[0,1] = ngram						# n-gram size
+	fp[0,1] = ngram_size				# n-gram size
 	fp[0,2] = len(word_to_id_dict)		# number of word types (MLP classes)
 	counter = 1
 	for line in data:
@@ -66,7 +75,7 @@ with open(tmp_path, 'r') as data:
 	fp.flush
 	del fp
 
-if output_path_txt != '-':
+if output_path_txt is not None:
 	shutil.move(tmp_path, output_path_txt)
 else:
 	os.remove(tmp_path)
