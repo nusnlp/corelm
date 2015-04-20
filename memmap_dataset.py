0a722d6fd0382c90b997e8e1af5b9186099e7cc4
Shamil CM <shamil.cm@gmail.com>
2015-04-20 20:57:07 +0800

    frequency based indices.

--- memmap_dataset.py
+++ memmap_dataset.py
@@ -44,8 +44,33 @@ nsamples = 0
 word_to_id_dict = dict()		# Word to Index Dictionary
 word_to_freq_dict = dict()		# Word Frequency Dictionary
 
-_, tmp_path = tempfile.mkstemp(prefix='dlm.tmp.')
+# Counting the frequency of the words in the first pass
+with open(input_path, 'r') as input_file:
+	for line in input_file:
+		line = line.strip()
+		if len(line) == 0:
+			continue
+		tokens = line.strip().split()
+		for token in tokens:
+			if not word_to_freq_dict.has_key(token):
+				word_to_freq_dict[token] = 1
+			else:
+				word_to_freq_dict[token] += 1
+				
 
+# Writing the vocab file and creating a word to id dictionary.
+word_to_id_dict['<unk>'] = 0
+word_to_id_dict['<s>'] = 1
+curr_index = 2
+with  open(vocab_path,'w') as f_vocab:
+	f_vocab.write('<unk>\t0\n<s>\t1\n')
+	for token in sorted(word_to_freq_dict, key=word_to_freq_dict.get, reverse=True):
+		f_vocab.write(token+"\t"+str(curr_index)+"\n")
+		word_to_id_dict[token]=curr_index
+		curr_index = curr_index + 1
+
+
+_, tmp_path = tempfile.mkstemp(prefix='dlm.tmp.')
 with open(input_path, 'r') as input_file, open(tmp_path, 'w') as tmp_file:
 	next_id = 0
 	for line in input_file:
@@ -57,21 +82,10 @@ with open(input_path, 'r') as input_file, open(tmp_path, 'w') as tmp_file:
 			tokens.insert(0, '<s>')
 		indices = []
 		for token in tokens:
-			if not word_to_id_dict.has_key(token):
-				word_to_id_dict[token] = str(next_id)
-				next_id += 1
-				word_to_freq_dict[token] = 1 
-			else:
-				word_to_freq_dict[token] += 1
-			indices.append(word_to_id_dict[token])
+			indices.append(str(word_to_id_dict[token]))
 		for i in range(ngram_size - 1, len(indices)):
 			tmp_file.write(' '.join(indices[i - ngram_size + 1 : i + 1]) + "\n")
 			nsamples += 1
-
-# Writing to the vocabulary file in decreasing order of frequency
-with  open(vocab_path,'w') as f_vocab:
-	for token in sorted(word_to_freq_dict, key=word_to_freq_dict.get, reverse=True):
-		f_vocab.write(token+"\t"+ word_to_id_dict.get(token)+"\n")
 
 with open(tmp_path, 'r') as data:
 	fp = np.memmap(output_path, dtype='int32', mode='w+', shape=(nsamples + 1, ngram_size))
