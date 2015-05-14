8b16bc3b24075e3f3f33b9c40f4fa97ed22b602b
Kaveh taghipour <kaveh.taghipour@gmail.com>
2015-05-14 18:48:13 +0800

    Added support for colored output

--- dlm/utils.py
+++ dlm/utils.py
@@ -35,6 +35,39 @@ def capture_output(command):
 	
 #-----------------------------------------------------------------------------------------------------------#
 	
+class BColors:
+	HEADER = '\033[95m'
+	OKBLUE = '\033[94m'
+	OKGREEN = '\033[92m'
+	WARNING = '\033[93m'
+	FAIL = '\033[91m'
+	ENDC = '\033[0m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
+	WHITE = '\033[37m'
+	YELLOW = '\033[33m'
+	GREEN = '\033[32m'
+	BLUE = '\033[34m'
+	CYAN = '\033[36m'
+	RED = '\033[31m'
+	MAGENTA = '\033[35m'
+	BLACK = '\033[30m'
+	BHEADER = BOLD + '\033[95m'
+	BOKBLUE = BOLD + '\033[94m'
+	BOKGREEN = BOLD + '\033[92m'
+	BWARNING = BOLD + '\033[93m'
+	BFAIL = BOLD + '\033[91m'
+	BUNDERLINE = BOLD + '\033[4m'
+	BWHITE = BOLD + '\033[37m'
+	BYELLOW = BOLD + '\033[33m'
+	BGREEN = BOLD + '\033[32m'
+	BBLUE = BOLD + '\033[34m'
+	BCYAN = BOLD + '\033[36m'
+	BRED = BOLD + '\033[31m'
+	BMAGENTA = BOLD + '\033[35m'
+	BBLACK = BOLD + '\033[30m'
+	OFF = BOLD + '\033[0,0m'
+
 def pprint(message, print_target="stdout"):
 	assert_value(print_target, ["stdout", "stderr"])
 	if print_target == "stderr":
@@ -46,21 +79,21 @@ def pprintln(message, print_target):
 	pprint(message + "\n", print_target)
 
 def error(message):
-	sys.stderr.write("[ERROR] " + message + "\n")
+	sys.stderr.write(BColors.BFAIL + "[ERROR] " + BColors.ENDC + message + "\n")
 	sys.exit()
 
 def warning(message):
-	sys.stderr.write("[WARNING] " + message + "\n")
+	sys.stderr.write(BColors.BWARNING + "[WARNING] " + BColors.ENDC + message + "\n")
 	
 def info(message):
-	sys.stderr.write("[INFO] " + message + "\n")
+	sys.stderr.write(BColors.BOKBLUE + "[INFO] " + BColors.ENDC + message + "\n")
 	
 def usage(message):
-	sys.stderr.write("[USAGE] " + message + "\n")
+	sys.stderr.write(BColors.BOLD + "[USAGE] " + BColors.ENDC + message + "\n")
 	sys.exit()
 
 def exception():
-	sys.stderr.write("[ERROR] " + str(sys.exc_info()[0].mro()[0].__name__) + ": " + sys.exc_info()[1].message + "\n")
+	sys.stderr.write(BColors.BFAIL + "[ERROR] " + BColors.ENDC + str(sys.exc_info()[0].mro()[0].__name__) + ": " + sys.exc_info()[1].message + "\n")
 	sys.exit()
 	
 #-----------------------------------------------------------------------------------------------------------#
@@ -89,12 +122,12 @@ def append_to_file(file_name, text):
 		f.write(text)
 
 def mkdir_p(path):
-    try:
-        os.makedirs(path)
-    except OSError as exc: # Python >2.5
-        if exc.errno == errno.EEXIST and os.path.isdir(path):
-            pass
-        else: raise
+	try:
+		os.makedirs(path)
+	except OSError as exc: # Python >2.5
+		if exc.errno == errno.EEXIST and os.path.isdir(path):
+			pass
+		else: raise
 
 def num_lines(path):
 	return sum(1 for line in open(path))
