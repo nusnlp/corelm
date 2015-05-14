ba8d600188aacc565fc7d654b69b1c03c69df87f
Kaveh taghipour <kaveh.taghipour@gmail.com>
2015-05-14 18:49:44 +0800

    Added support for multiple hidden layers and got rid of redundant code in logistic regression and hidden layer implementations.

--- dlm/models/ltmlp.py
+++ dlm/models/ltmlp.py
@@ -1,6 +1,6 @@
-from dlm.models.components.lookuptable import LookupTableLayer
-from dlm.models.components.hiddenlayer import HiddenLayer
-from dlm.models.components.logisticregression import LogisticRegression
+from dlm.models.components.lookuptable import LookupTable
+from dlm.models.components.linear import Linear
+from dlm.models.components.activation import Activation
 from dlm.models import classifier
 import dlm.utils as U
 import theano.tensor as T
@@ -10,67 +10,121 @@ class MLP(classifier.Classifier):
 
 	def __init__(self, args=None, model_path=None):
 
+		######################################################################
+		## Parameters
+		#
+		
 		U.xassert((args or model_path) and not (args and model_path), "args or model_path are mutually exclusive")
 		
 		if model_path:
 			args, loaded_params = self.load_model(model_path)
 		
-		self.args = args
 		emb_dim = args.emb_dim
 		num_hidden_list = map(int, args.num_hidden.split(','))
-		U.xassert(len(num_hidden_list) == 1, "More than one hidden layer is not supported yet")
+		for i in range(len(num_hidden_list)):
+			U.info("Hidden Layer %i: %i" % (i+1, num_hidden_list[i]))
 		vocab_size = args.vocab_size
-		self.ngram_size = args.ngram_size
+		ngram_size = args.ngram_size
 		num_classes = args.num_classes
+		activation_name = args.activation_name
+		self.args = args
+		self.L1 = 0
+		self.L2_sqr = 0
+		self.params = []
 		
-		# Creating model
-		
-		self.rng = numpy.random.RandomState(1234)
-		self.input = T.imatrix('input')		# the data is presented as rasterized images
+		rng = numpy.random.RandomState(1234)
+		self.input = T.imatrix('input')
 
-		self.lookupTableLayer = LookupTableLayer(
-			rng=self.rng,
+		######################################################################
+		## Lookup Table Layer
+		#
+		
+		lookupTableLayer = LookupTable(
+			rng=rng,
 			input=self.input,
 			vocab_size=vocab_size,
 			emb_dim=emb_dim
 		)
-
-		self.hiddenLayer = HiddenLayer(
-			rng=self.rng,
-			input=self.lookupTableLayer.output,
-			n_in=(self.ngram_size - 1) * emb_dim,
-			n_out=num_hidden_list[0],
-			activation=T.tanh
-		)
-
-		self.logRegressionLayer = LogisticRegression(
-			input=self.hiddenLayer.output,
-			n_in=num_hidden_list[-1],
+		last_layer_output = lookupTableLayer.output
+		last_layer_output_size = (ngram_size - 1) * emb_dim
+		self.params += lookupTableLayer.params
+		
+		######################################################################
+		## Hidden Layer(s)
+		#
+		
+		for i in range(0, len(num_hidden_list)):
+			linearLayer = Linear(
+				rng=rng,
+				input=last_layer_output,
+				n_in=last_layer_output_size,
+				n_out=num_hidden_list[i]
+			)
+			last_layer_output = linearLayer.output
+			last_layer_output_size = num_hidden_list[i]
+			self.params += linearLayer.params
+			
+			activation = Activation(
+				input=last_layer_output,
+				func_name=activation_name
+			)
+			last_layer_output = activation.output
+			
+			self.L1 = self.L1 + abs(linearLayer.W).sum()
+			self.L2_sqr = self.L2_sqr + (linearLayer.W ** 2).sum()
+		
+		######################################################################
+		## Output Linear Layer
+		#
+		
+		linearLayer = Linear(
+			rng=rng,
+			input=last_layer_output,
+			n_in=last_layer_output_size,
 			n_out=num_classes
 		)
-
-		self.L1 = (
-			abs(self.hiddenLayer.W).sum()
-			+ abs(self.logRegressionLayer.W).sum()
-		)
-
-		self.L2_sqr = (
-			(self.hiddenLayer.W ** 2).sum()
-			+ (self.logRegressionLayer.W ** 2).sum()
-		)
-
-		self.negative_log_likelihood = self.logRegressionLayer.negative_log_likelihood
+		last_layer_output = linearLayer.output
+		self.params += linearLayer.params
 		
-		self.p_y_given_x = self.logRegressionLayer.p_y_given_x
-
-		self.errors = self.logRegressionLayer.errors
-
-		self.params = self.lookupTableLayer.params + self.hiddenLayer.params + self.logRegressionLayer.params
+		self.L1 = self.L1 + abs(linearLayer.W).sum()
+		self.L2_sqr = self.L2_sqr + (linearLayer.W ** 2).sum()
 		
-		# Initializating parameters (if model file is given)
+		######################################################################
+		## Model Output
+		#
+		
+		self.p_y_given_x_matrix = T.nnet.softmax(last_layer_output)
+		
+		self.log_Z_sqr = T.log(T.mean(T.sum(T.exp(last_layer_output), axis=1))) ** 2
+		
+		######################################################################
+		## Model Predictions
+		#
+		
+		self.y_pred = T.argmax(self.p_y_given_x_matrix, axis=1)
+		
+		######################################################################
+		## Loading parameters from file (if given)
+		#
+		
 		if model_path:
 			self.set_params(loaded_params)
 		
+	######################################################################
+	## Model Functions
+	#
 		
+	def p_y_given_x(self, y):
+		return self.p_y_given_x_matrix[T.arange(y.shape[0]), y]
+	
+	def negative_log_likelihood(self, y):
+		return -T.mean(T.log(self.p_y_given_x(y)))
 		
-		
+	def errors(self, y):
+		if y.ndim != self.y_pred.ndim:
+			raise TypeError('y should have the same shape as self.y_pred', ('y', y.type, 'y_pred', self.y_pred.type))
+		if y.dtype.startswith('int'):
+			return T.sum(T.neq(self.y_pred, y))
+		else:
+			raise NotImplementedError()
+
