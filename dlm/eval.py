895a5b72f10c53e7d3248a13ef6fa3f166771bd6
Kaveh taghipour <kaveh.taghipour@gmail.com>
2015-05-29 17:47:52 +0800

    Added a theano function that gives the list of probabilities for all samples.

--- dlm/eval.py
+++ dlm/eval.py
@@ -12,42 +12,49 @@ import numpy as np
 class Evaluator():
 
 	def __init__(self, dataset, classifier):
-		self.dataset = dataset							# Initializing the dataset
-		self.num_batches = dataset.get_num_batches()	# Number of minibatches in the dataset
-		self.num_samples = dataset._get_num_samples()	# Number of samples in the dataset
-
+		
 		index = T.lscalar()
 		x = classifier.input
 		y = T.ivector('y')
 		
-		self.neg_sum_batch_log_likelihood = theano.function(
-			inputs=[index],
-			outputs=-T.sum(T.log(classifier.p_y_given_x(y))),
-			givens={
-				x: self.dataset.get_x(index),
-				y: self.dataset.get_y(index)
-			}
-		)
+		if dataset:
+			self.dataset = dataset								# Initializing the dataset
+			self.num_batches = self.dataset.get_num_batches()	# Number of minibatches in the dataset
+			self.num_samples = self.dataset._get_num_samples()	# Number of samples in the dataset
 		
-		self.sum_batch_error = theano.function(
-			inputs=[index],
-			outputs=classifier.errors(y),
-			givens={
-				x: dataset.get_x(index),
-				y: dataset.get_y(index)
-			}
-		)
+			self.neg_sum_batch_log_likelihood = theano.function(
+				inputs=[index],
+				outputs=-T.sum(T.log(classifier.p_y_given_x(y))),
+				givens={
+					x: self.dataset.get_x(index),
+					y: self.dataset.get_y(index)
+				}
+			)
 		
-		# x: A matrix (N * (ngram - 1)) representing the sequence of length N
-		# y: A vector of class labels
-		self.sequence_log_prob = self.neg_sum_batch_log_likelihood
+			self.sum_batch_error = theano.function(
+				inputs=[index],
+				outputs=classifier.errors(y),
+				givens={
+					x: self.dataset.get_x(index),
+					y: self.dataset.get_y(index)
+				}
+			)
 		
-		self.denominator = theano.function(
-			inputs=[index],
-			outputs=classifier.log_Z_sqr,
-			givens={
-				x: dataset.get_x(index)
-			}
+			# x: A matrix (N * (ngram - 1)) representing the sequence of length N
+			# y: A vector of class labels
+			self.neg_sequence_log_prob = self.neg_sum_batch_log_likelihood
+			
+			self.denominator = theano.function(
+				inputs=[index],
+				outputs=classifier.log_Z_sqr,
+				givens={
+					x: self.dataset.get_x(index)
+				}
+			)
+		
+		self.ngram_log_prob = theano.function(
+			inputs=[x, y],
+			outputs=T.log(classifier.p_y_given_x(y)),
 		)
 
 	def classification_error(self):
@@ -60,7 +67,10 @@ class Evaluator():
 		return math.exp(self.mean_neg_log_likelihood())
 
 	def get_sequence_log_prob(self, index):
-		return - self.sequence_log_prob(index)
+		return - self.neg_sequence_log_prob(index)
+	
+	def get_ngram_log_prob(self, x, y):
+		return self.ngram_log_prob(x, y)
 	
 	def get_denominator(self):
 		return np.mean([self.denominator(i) for i in xrange(self.num_batches)])
