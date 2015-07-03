f3bd6ed42742a77fdc0cb432e18106af9272d1e4
Shamil CM <shamil.cm@gmail.com>
2015-07-03 14:51:13 +0800

    LR Tuner limits are made relative to the learning rate

--- dlm/trainer.py
+++ dlm/trainer.py
@@ -20,14 +20,12 @@ def train(classifier, criterion, args, trainset, devset, testset=None):
 	num_train_batches = trainset.get_num_batches()
 	
 	trainer = Trainer(classifier, criterion, args.learning_rate, trainset)
-	lr_tuner = LRTuner(low=0.01, high=1, inc=0.01)
+	lr_tuner = LRTuner(low=0.01*args.learning_rate, high=10*args.learning_rate, inc=0.01*args.learning_rate)
 
 	validation_frequency = 5000 # minibatches
 	total_num_iter = args.num_epochs * num_train_batches
 	hook = Hook(classifier, devset, testset, total_num_iter, args.out_dir)
-	
 	L.info('Training')
-	
 	start_time = time.time()
 	verbose_freq = 1000 # minibatches
 	epoch = 0
@@ -37,32 +35,24 @@ def train(classifier, criterion, args, trainset, devset, testset=None):
 		L.info("Epoch: " + U.BColors.RED + str(epoch) + U.BColors.ENDC)
 		minibatch_avg_cost_sum = 0
 		for minibatch_index in xrange(num_train_batches):
-		#for minibatch_index in range(100):
 			minibatch_avg_cost = trainer.step(minibatch_index)
 			minibatch_avg_cost_sum += minibatch_avg_cost
 			
 			if minibatch_index % verbose_freq == 0:
 				L.info(U.BColors.BLUE + "[" + time.ctime() + "] " + U.BColors.ENDC + '%i/%i, cost=%.2f, lr=%f'
 					% (minibatch_index, num_train_batches, minibatch_avg_cost_sum/(minibatch_index+1), trainer.get_learning_rate()))
-
 			curr_iter = (epoch - 1) * num_train_batches + minibatch_index
 			if curr_iter > 0 and curr_iter % validation_frequency == 0:
 				hook.evaluate(curr_iter)
-		
 		dev_ppl = hook.evaluate(curr_iter)
 		lr = trainer.get_learning_rate()
 		lr = lr_tuner.adapt_lr(dev_ppl, lr)
 		trainer.set_learning_rate(lr)
 
 	end_time = time.time()
-	
 	hook.evaluate(total_num_iter)
-
 	L.info('Optimization complete')
 	L.info('Ran for %.2fm' % ((end_time - start_time) / 60.))
-
-
-
 
 
 class Hook:
@@ -78,7 +68,7 @@ class Hook:
 		self.t0 = time.time()
 		self.total_num_iter = total_num_iter
 		self.out_dir = out_dir
-	
+
 	def evaluate(self, curr_iter):
 		denominator = self.dev_eval.get_denominator()
 		dev_error = self.dev_eval.classification_error()
@@ -95,7 +85,7 @@ class Hook:
 
 		t1 = time.time()
 		rem_time = int((self.total_num_iter - curr_iter) * (t1 - self.t0) / curr_iter)
-	
+
 		L.info(('DEV =>  Error=%.2f%%, PPL=' + U.BColors.BYELLOW + '%.2f' + U.BColors.ENDC + ' (best=' + U.BColors.BRED + '%.2f @ %i' + U.BColors.ENDC + '), Denom=%.3f, %im')
 			% (dev_error * 100., dev_perplexity, self.best_dev_perplexity, self.best_iter, denominator, rem_time / 60))
 		if self.test_eval:
