691a5d01d1de3029ce9eef7d8f48c1d6a4ae6dda
Shamil CM <shamil.cm@gmail.com>
2015-07-03 17:29:48 +0800

    NCE Implementation done.

--- dlm/criterions/nce.py
+++ dlm/criterions/nce.py
@@ -3,42 +3,42 @@ from theano.tensor.shared_randomstreams import RandomStreams
 from theano import function
 import numpy as np
 import theano
+import math
 
 class NCELikelihood():
 
 	def __init__(self, classifier, args, noise_dist):
-		theano.config.exception_verbosity = 'high'	
 		self.y = T.ivector('y')
-		
-		## Generating noise samples 
-		srng = RandomStreams(seed=1234)  
-		#args.num_noise_samples = 1
-		#noise_samples = T.nonzero(srng.multinomial(size=(args.num_noise_samples,y.shape[0]), pvals=noise_dist))[1]
-		noise_samples = T.nonzero(srng.multinomial(size=(self.y.shape[0],args.num_noise_samples), pvals=noise_dist))[2].reshape((self.y.shape[0],args.num_noise_samples))
-		
-		self.noise_dist = noise_dist
-		self.batchsize = args.batchsize
-		self.num_noise_samples = args.num_noise_samples
-		self.noise_samples = np.zeros((args.batchsize,args.num_noise_samples))
 		
 		## Cost function
 		#  Sum over minibatch instances (log ( u(w|c) / (u(w|c) + k * p_n(w)) ) + sum over noise samples ( log ( u(x|c) / ( u(x|c) + k * p_n(x) ) )))
+		
+		# Generating noise samples
+		srng = RandomStreams(seed=1234)
+		noise_samples = srng.choice(size=(self.y.shape[0],args.num_noise_samples),  a=args.num_classes, p=noise_dist, dtype='int32')
 
-		data_scores = T.exp(classifier.output[T.arange(self.y.shape[0]),self.y]) # u(w|c) for all w in y (Shape: 1 x #instances)
-		log_data = T.log(data_scores / ( data_scores + args.num_noise_samples * noise_dist[self.y] )) # log ( u(w|c) / (u(w|c) + k * p_n(w))
-		
-		noise_prob = noise_dist[noise_samples]									   # p_n(x) for all noise samples (Shape: #instances x k)
-		noise_mass = args.num_noise_samples * noise_prob								   # k * p_n(x) for all noise samples (Shape: #instance x k)
-		noise_scores = 	T.exp(classifier.output[T.arange(noise_samples.shape[0]).reshape((-1,1)),noise_samples]) # u(x|c) (Shape: #instances x k)
-		noise_log_sum = T.sum( T.log(noise_mass / (noise_scores + noise_mass)) , axis=1) #Sum(log ( u(x|c) /(u(x|c) + k * p_n(x))) (Shape: 1 x #instances)
-		
+		log_noise_dist = theano.shared(np.log(noise_dist.get_value()),borrow=True)
+		log_num_noise_samples = math.log(args.num_noise_samples)
+
+		# Data Part of Cost Function: log ( u(w|c) / (u(w|c) + k * p_n(w))
+		data_scores = classifier.output[T.arange(self.y.shape[0]),self.y]
+		data_denom = self.logadd(data_scores, log_num_noise_samples + log_noise_dist[self.y] )
+		data_prob = data_scores - data_denom
+		# Sumation of Noise Part of Cost Function: sum over noise samples ( log ( u(x|c) / ( u(x|c) + k * p_n(x) ) ))
+		noise_mass = log_num_noise_samples + log_noise_dist[noise_samples] # log(k) + log(p_n(x)) for all noise samples (Shape: #instaces x k)
+		noise_scores = classifier.output[T.arange(noise_samples.shape[0]).reshape((-1,1)),noise_samples]
+		noise_denom = self.logadd(noise_scores, noise_mass)
+		noise_prob_sum = T.sum(noise_mass - noise_denom, axis=1)
+
 		self.cost = (
-			T.mean(log_data + noise_log_sum) 
-	
+			-T.mean(data_prob + noise_prob_sum)
 		)
-	
-	def generate_noise(self):
-		self.noise_samples = np.nonzero(np.random.multinomial(1,self.noise_dist.get_value(), size=(self.batchsize,self.num_noise_samples)))[2].reshape(self.batchsize,self.num_noise_samples)
+		self.test = (
+			T.sum(data_scores)
+		)
 
-
+	def logadd(self, a, b):
+		g = T.maximum(a,b)
+		l = T.minimum(a,b)
+		return g + T.log(1 + T.exp(l-g))
 
