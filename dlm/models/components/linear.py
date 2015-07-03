e715dd65d3adf74624bc2102afd6a6d8f8706da6
Shamil CM <shamil.cm@gmail.com>
2015-07-03 16:17:02 +0800

    Initialize from argument or using a uniform distribution

--- dlm/models/components/linear.py
+++ dlm/models/components/linear.py
@@ -4,11 +4,11 @@ import theano.tensor as T
 
 class Linear():
 
-	def __init__(self, rng, input, n_in, n_out, W=None, b=None):
+	def __init__(self, rng, input, n_in, n_out, W_values=None, b_values=None):
 
 		self.input = input
 
-		if W is None:
+		if W_values is None:
 			W_values = numpy.asarray(
 				rng.uniform(
 					low = -0.01, #low=-numpy.sqrt(6. / (n_in + n_out)),
@@ -17,11 +17,19 @@ class Linear():
 				),
 				dtype=theano.config.floatX
 			)
-			W = theano.shared(value=W_values, name='W', borrow=True)
+		W = theano.shared(value=W_values, name='W', borrow=True)
 
-		if b is None:
+		if b_values is None:
 			b_values = numpy.zeros((n_out,), dtype=theano.config.floatX)
-			b = theano.shared(value=b_values, name='b', borrow=True)
+		#	b_values = numpy.asarray(
+		#		rng.uniform(
+		#			low = -0.01, #low=-numpy.sqrt(6. / (n_in + n_out)),
+		#			high = 0.01, #high=numpy.sqrt(6. / (n_in + n_out)),
+		#			size=(n_out,)
+		#		),
+		#		dtype=theano.config.floatX
+			)
+		b = theano.shared(value=b_values, name='b', borrow=True)
 
 		self.W = W
 		self.b = b
