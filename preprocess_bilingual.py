f2ced1b57a64f1e1ad754f2342fec7e80bf55e03
Shamil CM <shamil.cm@gmail.com>
2015-08-05 14:03:46 +0800

    Bilingual language modelling preprocessing

--- preprocess_bilingual.py
+++ preprocess_bilingual.py
@@ -43,6 +43,22 @@ def process_vocab(input_path, vocab_size, vocab_path):
 			curr_index = curr_index + 1
 	return word_to_id_dict
 
+def read_vocab(vocab_path, endp):
+	word_to_id_dict = dict()
+	with open(vocab_path,'r') as f_vocab:
+		curr_index = 0
+		for line in f_vocab:
+			token = line.strip()
+			if not word_to_id_dict.has_key(token):
+				word_to_id_dict[token] = curr_index
+			curr_index = curr_index + 1
+		U.xassert(word_to_id_dict.has_key('<s>') and word_to_id_dict.has_key('<unk>'), "Missing <s> or <unk> in given vocab file")
+		if endp:
+			U.xassert(word_to_id_dict.has_key('</s>'), "Missing </s> in given vocab file while --endp flag is used")
+		if word_to_id_dict.has_key('</s>'):
+			U.xassert(args.endp, "Given vocab file has </s> but --endp flag is not activated")
+	return word_to_id_dict
+
 def replace_unks(tokens, word_to_id_dict):
 	replaced_tokens = []
 	for token in tokens:
@@ -57,11 +73,6 @@ parser.add_argument("-is", "--input-source-text", dest="src_input_path", require
 parser.add_argument("-it", "--input-target-text", dest="trg_input_path", required=True, help="Path to the target language training text file")
 parser.add_argument("-ia", "--alignment-file", dest="alignment_path", required=True, help="Alignment file for training text")
 
-#TODO: Make validation source tex, target text and aligment file a single unit
-parser.add_argument("-ts", "--tune-source-text", dest="src_tune_path",  help="Path to the source langauge tuning text file")
-parser.add_argument("-tt", "--tune-target-text", dest="trg_tune_path",  help="Path to the target language tuning text file")
-parser.add_argument("-ta", "--tune-alignment-file", dest="alignment_tune_path", help="Path to the alignment file for tuning text")
-
 parser.add_argument("-cs", "--source-context", dest="src_context", required=True, type=int, help="(Size of source context window - 1)/ 2")
 parser.add_argument("-ct", "--target-context", dest="trg_context", required=True, type=int, help="Size of target context window")
 
@@ -71,9 +82,17 @@ parser.add_argument("--shuffle", dest="shuffle", action='store_true', help="Add 
 parser.add_argument("--endp", dest="endp", action='store_true', help="Add this flag to add sentence end padding </s>")
 parser.add_argument("--word-output", dest="word_out", action='store_true', help="Get output in non-index format, i.e. as ngrams")
 
-parser.add_argument("-vs","--prune-source-vocab", dest="src_vocab_size", default=16000, type=int, help="Source Vocabulary size")
-parser.add_argument("-vt","--prune-target-vocab", dest="trg_vocab_size", default=16000, type=int, help="Target Vocabulary size")
-parser.add_argument("-vo","--prune-output-vocab", dest="output_vocab_size", type=int, help="Output Vocabulary size. Defaults to target vocabulary size.")
+src_prune_args = parser.add_mutually_exclusive_group(required=True)
+src_prune_args.add_argument("-vs","--prune-source-vocab", dest="src_vocab_size",  type=int, help="Source vocabulary size")
+src_prune_args.add_argument("--source-vocab-file", dest="src_vocab_path",  help="Source vocabulary file path")
+
+trg_prune_args = parser.add_mutually_exclusive_group(required=True)
+trg_prune_args.add_argument("-vt","--prune-target-vocab", dest="trg_vocab_size", type=int, help="Target vocabulary size")
+trg_prune_args.add_argument("--target-vocab-file", dest="trg_vocab_path", help="Target vocabulary file path")
+
+output_prune_args = parser.add_mutually_exclusive_group(required=True)
+output_prune_args.add_argument("-vo","--prune-output-vocab", dest="output_vocab_size", type=int, help="Output vocabulary size. Defaults to target vocabulary size.")
+output_prune_args.add_argument("--output-vocab-file", dest="output_vocab_path", help="Output vocabulary file")
 
 args = parser.parse_args()
 
@@ -91,23 +110,37 @@ L.info("Output directory: " + os.path.abspath(args.output_dir_path))
 # Prefix of files
 src_prefix = args.output_dir_path + "/" + os.path.basename(args.src_input_path)
 trg_prefix = args.output_dir_path + "/" + os.path.basename(args.trg_input_path)
+
+prefix = os.path.basename(args.src_input_path).split('.')[0]
+
 output_prefix = args.output_dir_path + "/output"
 
 # File paths
 if args.shuffle:
-	output_mmap_path = args.output_dir_path + "/train.idx.shuf.mmap"
-	output_idx_path = args.output_dir_path + "/train.idx.shuf.txt"
-	output_ngrams_path = args.output_dir_path + "/train.shuf.txt"
+	output_mmap_path = args.output_dir_path + "/" + prefix + ".idx.shuf.mmap"
+	output_idx_path = args.output_dir_path + "/" + prefix + ".idx.shuf.txt"
+	output_ngrams_path = args.output_dir_path + "/" + prefix + ".shuf.txt"
 else:
-	output_mmap_path = args.output_dir_path + "/train.idx.mmap"
-	output_idx_path = args.output_dir_path + "/train.idx.txt"
-	output_ngrams_path = args.output_dir_path + "/train.txt"
+	output_mmap_path = args.output_dir_path + "/" + prefix + ".idx.mmap"
+	output_idx_path = args.output_dir_path + "/" +  prefix + ".idx.txt"
+	output_ngrams_path = args.output_dir_path + "/" + prefix + ".txt"
 
 tune_output_path = "tune.idx.mmap"
 
-src_word_to_id = process_vocab(args.src_input_path, args.src_vocab_size, src_prefix+'.vocab')	# Word to index dictionary of source langauge
-trg_word_to_id = process_vocab(args.trg_input_path, args.trg_vocab_size, trg_prefix+'.vocab')	# Word to index dictionary of target langauge
-output_word_to_id = process_vocab(args.trg_input_path, args.output_vocab_size, output_prefix+'.vocab') # Word to index dictionary of output vocab
+if args.src_vocab_path is None:
+	src_word_to_id = process_vocab(args.src_input_path, args.src_vocab_size, src_prefix+'.vocab')	# Word to index dictionary of source langauge
+else:
+	src_word_to_id = read_vocab(args.src_vocab_path,args.endp)	
+
+if args.trg_vocab_path is None:
+	trg_word_to_id = process_vocab(args.trg_input_path, args.trg_vocab_size, trg_prefix+'.vocab')	# Word to index dictionary of target langauge
+else:
+	trg_word_to_id = read_vocab(args.trg_vocab_path, args.endp)
+
+if args.output_vocab_path is None:
+	output_word_to_id = process_vocab(args.trg_input_path, args.output_vocab_size, output_prefix+'.vocab') # Word to index dictionary of vocab
+else:
+	output_word_to_id = read_vocab(args.output_vocab_path, args.endp)
 
 svocab = len(src_word_to_id)
 tvocab = len(trg_word_to_id)
@@ -217,11 +250,12 @@ ngram_size = args.trg_context + args.src_context * 2 + 1
 
 # Creating the memory-mapped file
 with open(tmp_path, 'r') as data:
-	fp = np.memmap(output_mmap_path, dtype='int32', mode='w+', shape=(nsamples + 1, ngram_size))
+	fp = np.memmap(output_mmap_path, dtype='int32', mode='w+', shape=(nsamples + 3, ngram_size))
 	fp[0,0] = nsamples												# number of samples
 	fp[0,1] = ngram_size											# n-gram size
-	fp[0,2] = ovocab												# number of word types (MLP classes)
-	counter = 1
+	fp[1,0] = svocab + tvocab										# context vocab size
+	fp[2,0] = ovocab												# output vocab size
+	counter = 3
 	for line in data:
 		tokens = line.split()
 		fp[counter] = tokens
