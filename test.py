63faa9abf03e3ff71e4294bea4a7301fb2890cbb
Shamil Chollampatt <shamil.cm@gmail.com>
2016-06-09 15:13:31 +0800

    Testing for sequence labelling task

--- test.py
+++ test.py
@@ -5,21 +5,23 @@ import time
 import argparse
 import dlm.utils as U
 import dlm.io.logging as L
-
+from dlm.io.vocabReader import VocabManager
 ###############
 ## Arguments
 #
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-t", "--test-file", dest="test_path", required=True, help="The evaluation file (memory-mapped, nbest list or text file)")
-parser.add_argument("-f", "--format", dest="format", required=True, help="The evaluation file format (mmap|nbest|text)")
+parser.add_argument("-f", "--format", dest="format", required=True, help="The evaluation file format (fmmap|mmap|nbest|text)")
 parser.add_argument("-v", "--vocab-file", dest="vocab_path", help="The vocabulary file that was used in training")
 parser.add_argument("-m", "--model-file", dest="model_path", required=True, help="Input PrimeLM model file")
 parser.add_argument("-ulp", "--unnormalized-log-prob-file", dest="ulp_path", help="Output file for sentence-level UNNORMALIZED log-probabilities")
 parser.add_argument("-nlp", "--normalized-log-prob-file", dest="nlp_path", help="Output file for sentence-level NORMALIZED log-probabilities")
 parser.add_argument("-ppl", "--perplexity", action='store_true', help="Compute perplexity")
+parser.add_argument("-op", "--output_path", dest="out_path",  help="Output classes path")
 parser.add_argument("-un", "--unnormalized", action='store_true', help="Output need not be normalized")
 parser.add_argument("-d", "--device", dest="device", default="gpu", help="The computing device (cpu or gpu)")
+
 args = parser.parse_args()
 
 U.set_theano_device(args.device, 1)
@@ -39,13 +41,18 @@ classifier = MLP(model_path=args.model_path)
 ## Loading dataset
 #
 
-U.xassert(args.format == "mmap" or args.format == "nbest" or args.format == "text", "Invalid file format given: " + args.format)
+U.xassert(args.format == "mmap" or args.format == "nbest" or args.format == "text" or args.format == "fmmap", "Invalid file format given: " + args.format)
 U.xassert(args.perplexity or args.nlp_path or args.ulp_path, "You should use one of (or more) -ppl, -nlp or -ulp")
 
 if args.format == "mmap":
 	U.xassert((args.nlp_path is None) and (args.ulp_path is None), "Cannot compute log-probabilities for an mmap file")
 	from dlm.io.mmapReader import MemMapReader
 	testset = MemMapReader(dataset_path=args.test_path, batch_size=500)
+elif args.format == "fmmap":
+	U.xassert((args.nlp_path is None) and (args.ulp_path is None), "Cannot compute log-probabilities for an features mmap file")
+	from dlm.io.featuresmmapReader import FeaturesMemMapReader
+	testset = FeaturesMemMapReader(dataset_path=args.test_path, batch_size=500)
+
 else:
 	U.xassert(args.vocab_path, "Vocab file is required for non-mmap file formats")
 	from dlm.io.textReader import TextReader
@@ -76,10 +83,19 @@ if args.nlp_path:
 		for i in xrange(testset.get_num_sentences()):
 			output.write(str(evaluator.get_sequence_log_prob(i)) + '\n')
 
+
 if args.ulp_path:
 	with open(args.ulp_path, 'w') as output:
 		for i in xrange(testset.get_num_sentences()):
 			output.write(str(evaluator.get_unnormalized_sequence_log_prob(i)) + '\n')
+
+if args.out_path:
+	with open(args.out_path, 'w') as output:
+		for i in xrange(testset.get_num_batches()):
+			batch_labels = evaluator.get_batch_predicted_class(i)
+			for label in batch_labels:
+				output.write(str(label) + '\n')
+
 
 L.info("Ran for %.2fs" % (time.time() - start_time))
 
